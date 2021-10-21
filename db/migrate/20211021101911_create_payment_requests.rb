166d9a9c2f2722588a723e7ed8d637300bfc47bd
Aayush Jain <aayushj.pis@gmail.com>
2021-10-21 18:00:50 +0530

    Added bunny gem for microservices inter communication

---
+++ db/migrate/20211021101911_create_payment_requests.rb
@@ -0,0 +1,12 @@
+class CreatePaymentRequests < ActiveRecord::Migration[6.1]
+  def change
+    create_table :payment_requests do |t|
+      t.integer :amount
+      t.string :currency
+      t.string :description
+      t.integer :status
+
+      t.timestamps
+    end
+  end
+end
